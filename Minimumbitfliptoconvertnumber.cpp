7d9494246be851f25fc77d48f43d5a2eca9ad121
Srasti Bhardwaj <srastibhardwaj72@gmail.com>
2024-09-11 22:55:55 +0530

    problem32

---
+++ Minimumbitfliptoconvertnumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int res=start^goal;
+        int count=0;
+        while(res) {
+            count+=res&1;
+            res>>=1;
+        }
+        return count;
+        
+    }
+};

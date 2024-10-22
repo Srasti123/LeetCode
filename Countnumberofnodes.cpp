4710a139711e4f2b38d55bea67edce78a21b8ebd
Srasti Bhardwaj <srastibhardwaj72@gmail.com>
2024-10-22 11:01:23 +0530

    problem65

---
+++ Countnumberofnodes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+int countNodes(TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+TreeNode* createTree() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+    return root;
+}
+
+int main() {
+    TreeNode* root = createTree();
+    int nodeCount = countNodes(root);
+    cout << "Number of nodes in the tree: " << nodeCount << endl;
+
+    return 0;
+}

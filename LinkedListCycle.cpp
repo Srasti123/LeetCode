5ba9eaf0457c93ebf5204e61a52db40a7bfd3cd3
Srasti Bhardwaj <srastibhardwaj72@gmail.com>
2024-12-04 00:21:48 +0530

    problem116

---
+++ LinkedListCycle.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+  int data;
+  Node* next;
+  Node(int data1, Node* next1) {
+    data = data1;
+    next = next1;
+  }
+  Node(int data1) {
+    data = data1;
+    next = nullptr;
+  }
+
+};
+bool detectCycle(Node* head) {
+  Node* slow = head;
+  Node* fast = head;
+  while (fast != nullptr && fast->next != nullptr ) {
+    slow = slow->next;
+    fast = fast->next->next;
+    if (slow == fast) {
+      return true;
+    }
+  }
+  return false;
+}
+int main() {
+  Node* head = new Node(1);
+  Node* second = new Node(2);
+  Node* third = new Node(3);
+  Node* fourth = new Node(4);
+  Node* fifth = new Node(5);
+  head->next = second;
+  second->next = third;
+  third->next = fourth;
+  fourth->next = fifth;
+  fifth->next = third;
+  if (detectCycle(head)) {
+    cout << "Loop detected in the Linked List"<<endl;
+  } else {
+    cout << "No loop detected in the Linked List"<<endl;
+  }
+  return 0;
+}
